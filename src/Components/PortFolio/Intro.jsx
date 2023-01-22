1058e9379de749cd623b75791d554916de7c9dd1
waqas1226 <112298023+waqas1226@users.noreply.github.com>
2023-01-22 16:13:29 +0000

    site response

--- src/Components/PortFolio/Intro.jsx
+++ src/Components/PortFolio/Intro.jsx
@@ -9,7 +9,7 @@ function Intro(props) {
             <span className="greet">Hi</span>
             <h1>I'M <span className="o">Waqas</span></h1>
             <p className="desc">I am a beginner developer having passion about learning and working with ReactJS. 
-            I am working in web development technologies, including HTML, CSS, and JavaScript.
+            I am working in web development technologies, including ReactJs, HTML, CSS, and JavaScript.
              Additionally, I have a basic understanding of the MERN stack (MongoDB, ExpressJS, ReactJS, and NodeJS), which is a popular technology stack for building web applications. 
              I am also familiar with Bootstrap, and Material UI, Styled Components which are popular tools and libraries for building responsive and user-friendly web interfaces and have experience working with Firebase.  
             I have created some projects which are described in <a href="#projects">My Projects</a> section.   
